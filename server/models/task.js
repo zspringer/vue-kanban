32fd01210f340add5212a990ab82b97855691083
Dee Foster <dee3724f@gmail.com>
2017-08-31 17:20:05 -0600

    bup

--- server/models/task.js
+++ server/models/task.js
@@ -7,8 +7,8 @@ var schema = new mongoose.Schema({
 	description: { type: String },
 	created: { type: Number, default: Date.now() },
 	// Relations
-    boardId: { type: ObjectId, ref: models.board, required: true },
-    listId: { type: ObjectId, ref: models.list, required: true }
+    boardId: { type: ObjectId, ref: models.board.name, required: true },
+    listId: { type: ObjectId, ref: models.list.name, required: true }
 });
 
 module.exports = mongoose.model(models.task.name, schema);
