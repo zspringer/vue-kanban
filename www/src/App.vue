037b3c6f73c68ed55463059ee4c972f408378dd1
Zach Springer <zspringer.springer@gmail.com>
2017-09-01 16:22:07 -0600

    potential fix to navbar

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-
-    <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+    <div v-if="!activeUser" class="loggedout">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>                        
               </button>
-          <a class="navbar-brand" href="#">Vue Kanban</a>
-        </div>
+            <a class="navbar-brand" href="#">Vue Kanban</a>
+          </div>
 
 
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <!-- <li class="active"><a href="#">Home</a></li>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <!-- <li class="active"><a href="#">Home</a></li>
              <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
               <ul class="dropdown-menu">
@@ -26,47 +26,95 @@
             </li>
             <li><a href="#">Page 2</a></li>
             <li><a href="#">Page 3</a></li>  -->
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <div v-if="!activeUser" class="nonSal">
-              <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
-              <li class="dropdown">
-                <!-- v-if="!activeUser" -->
-                <a class="dropdown-toggle glyphicon glyphicon-user" data-toggle="dropdown"> Register<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <form @submit.prevent="login()">
-                    <input type="text" placeholder="name" v-model="accountUser.name">
-                    <input type="text" placeholder="email" v-model="accountUser.email">
-                    <input type="text" placeholder="password" v-model="accountUser.password">
-                    <li><button type="submit" class="btn" @click="register()">Submit</button></li>
-                  </form>
-                </ul>
-              </li>
-              <li class="dropdown">
-                <!-- v-if="!activeUser" -->
-                <a class="dropdown-toggle glyphicon glyphicon-log-in" data-toggle="dropdown"> Login<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <form @submit.prevent="login()">
-                    <input type="text" placeholder="email" v-model="accountUser.email">
-                    <input type="text" placeholder="password" v-model="accountUser.password">
-                    <li><button type="submit" class="btn" @click="login()">Log In</button></li>
-                  </form>
-                </ul>
-              </li>
-            </div>
-            <div v-else="" class="salutation">
-              <!-- v-if="activeUser" -->
-              <li class="idiot">{{activeUser.name}}</li>
-              <li> <button v-if="activeUser" class="getout" @click="logOut()">Log Out</button></li>
-            </div>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <div v-if="!activeUser" class="nonSal">
+                <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+                <li class="dropdown">
+                  <!-- v-if="!activeUser" -->
+                  <a class="dropdown-toggle glyphicon glyphicon-user" data-toggle="dropdown"> Register<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <form @submit.prevent="login()">
+                      <input type="text" placeholder="name" v-model="accountUser.name">
+                      <input type="text" placeholder="email" v-model="accountUser.email">
+                      <input type="text" placeholder="password" v-model="accountUser.password">
+                      <li><button type="submit" class="btn" @click="register()">Submit</button></li>
+                    </form>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <!-- v-if="!activeUser" -->
+                  <a class="dropdown-toggle glyphicon glyphicon-log-in" data-toggle="dropdown"> Login<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <form @submit.prevent="login()">
+                      <input type="text" placeholder="email" v-model="accountUser.email">
+                      <input type="text" placeholder="password" v-model="accountUser.password">
+                      <li><button type="submit" class="btn" @click="login()">Log In</button></li>
+                    </form>
+                  </ul>
+                </li>
+              </div>
+              <div v-else="" class="salutation">
+                <!-- v-if="activeUser" -->
+                <li class="idiot">{{activeUser.name}}</li>
+                <li> <button v-if="activeUser" class="getout" @click="logOut()">Log Out</button></li>
+              </div>
 
 
-            <!-- <button @click="logOut()">Log Out</button> -->
-            <!-- <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
-          </ul>
+              <!-- <button @click="logOut()">Log Out</button> -->
+              <!-- <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
+    <!-- NavBar for if user is logged in -->
+
+    <div v-if="activeUser" class="loggedin">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+              </button>
+            <a class="navbar-brand" href="#">Vue Kanban</a>
+          </div>
+
+
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <!-- <li class="active"><a href="#">Home</a></li>
+             <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Page 1-1</a></li>
+                <li><a href="#">Page 1-2</a></li>
+                <li><a href="#">Page 1-3</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>  -->
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <div class="nonSal">
+                <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+              </div>
+              <div class="salutation">
+                <!-- v-if="activeUser" -->
+                <li class="idiot">{{activeUser.name}}</li>
+                <li> <button class="getout" @click="logOut()">Log Out</button></li>
+              </div>
+
+
+              <!-- <button @click="logOut()">Log Out</button> -->
+              <!-- <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
 
 
 
@@ -144,8 +192,8 @@
     color: black;
   }
 
-router-link:link{
-    color:black;
+  router-link:link {
+    color: black;
   }
 
   #app {
@@ -181,7 +229,6 @@ router-link:link{
 
   .firstRow {
     height: 4%;
-
   }
 
   #app {
