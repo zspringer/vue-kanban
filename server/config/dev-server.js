923b3c368065d1eb50011036a3bf0cc280f349a9
Dee Foster <dee3724f@gmail.com>
2017-09-01 11:41:36 -0600

    update

--- server/config/dev-server.js
+++ server/config/dev-server.js
@@ -17,7 +17,7 @@ let server = require('http').createServer(app)
 function Validate(req, res, next) {
     // ONLY ALLOW GET METHOD IF NOT LOGGED IN 
     console.log(req.session)
-    if (req.method !== 'GET' && !req.session.uid) {
+    if (req.method !== 'GET' && !req.session.uid) { //req.method !== 'GET' && 
         return res.send({ error: 'Please Login or Register to continue' })
     }
     return next()
